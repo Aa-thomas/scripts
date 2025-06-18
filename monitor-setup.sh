c669f09341167cb3a5da0afc30829217bd841e5a
Aa-thomas <aarthomas01@gmail.com>
2025-06-18 19:55:15 -0400

    fix: correct monitor layout - DP-3 left primary, DP-1 right secondary
    - DP-3 (Monitor 2): LEFT side, PRIMARY display
    - DP-1 (Monitor 1): RIGHT side, secondary display
    - Fixes physical monitor arrangement to match user preference

--- monitor-setup.sh
+++ monitor-setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-xrandr --output DP-1 --auto --left-of DP-3 \
-       --output DP-3 --primary --auto
+xrandr --output DP-3 --primary --auto --left-of DP-1 \
+       --output DP-1 --auto
 
